0db903860c770719d939131ef7109f929edbfa50
Janardhan Yadav <deepy125@gmail.com>
2023-08-12 00:36:03 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+package Trials;
+
+public class Main{
+    public static class A{
+        public String meth(){
+       return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[]args){
+        B b = new B();
+        b.meth();
+
+        B b1 = new B();
+        b1.meth();
+    }
+}
